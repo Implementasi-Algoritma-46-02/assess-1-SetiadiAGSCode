07bc41c421facfe27de469fdda5423b9eed40152
SetiadiAgusnawan <setiadi.a130905@gmail.com>
2024-10-29 17:26:48 +0700

    test

--- Soal01.java
+++ Soal01.java
@@ -1,6 +1,31 @@
+import java.util.Scanner;
+
 public class Soal01 {
 
 	public static void main(final String[] args) {
 		// Kerjakan soalnya di sini
+		Scanner input = new Scanner(System.in);
+		
+		 int a = 123;
+		 int b = 456;
+		 int c = 789;
+		 int d = 101;
+		 int e = 135;
+
+		 if (a % 2 == 0 );
+		  System.out.println("Bilangan Ganjil");
+		 if (b % 2 == 0 );
+		  System.out.println("Bilangan Ganjil");
+		if (c % 2 == 0);
+		  System.out.println("Bilangan Ganjil");
+        if (d % 2 == 0);
+		  System.out.println("Bilangan Ganjil");
+		if (e % 2 == 0);
+		  System.out.println("Bilangan Ganjil");
+		
+		  System.out.println("Bilangan Genap");    
+
+
+		
 	}
 }
